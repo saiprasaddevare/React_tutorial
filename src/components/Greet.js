d454f8b2cd6052fd1f74824e6fa16bf21cd53492
saiprasaddevare <saiprasaddevare12@gmail.com>
2019-07-12 16:31:08 +0530

    React_Tutorial :: Functional Component Arrow function: Replace JS function with arrow => function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-function Greet() {
-  return <h1>Hello saiprasad</h1>
-}
+// function Greet() {
+//   return <h1>Hello saiprasad</h1>
+// }
+
+// This is another way of writing of above simple function i.e. arrow function
+const Greet = () => <h1>Hello Saiprasad</h1>
 
 export default Greet
